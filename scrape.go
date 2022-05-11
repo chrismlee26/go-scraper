78492e79736043e7d1f613d419f2427ec135ec9a
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-11 12:54:45 -0700

    [test] images links loading, wont download, might need parse

--- scrape.go
+++ scrape.go
@@ -9,25 +9,26 @@ import (
 	"github.com/gocolly/colly"
 )
 
+//  ---------------------
 // Struct for all URL's
+//  ---------------------
 type Url struct {
 	Text string
 	Link string
 }
 
-// Helper Functions
-
+//  ---------------------
 // Check Error
+//  ---------------------
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-// JSON Conversion
-
-// Append to File
-
+//  ---------------------
+// Main()
+//  ---------------------
 func main() {
 	// Create Slice to store URLs, length 100
 	var appendLinks = make([]Url, 100)
@@ -40,18 +41,18 @@ func main() {
 
 	// Instantiate default collector
 	c := colly.NewCollector(
-		colly.AllowedDomains("thesislabs.com"),
+		colly.AllowedDomains("thesislabs.com", "images.squarespace-cdn.com"),
 	)
 
 	// Limit Rules to prevent getting banned by the site
 	c.Limit(&colly.LimitRule{
-		DomainGlob: "*thesislabs.com",
+		DomainGlob: "thesislabs.com*",
 		Delay:      2 * time.Second,
 	})
 
 	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
 		src := e.Attr("src")
-		fmt.Printf("Image found: %q => %s", e.Text, src)
+		fmt.Printf("Image found: %q => %s \n", e.Text, src)
 
 		images = append(images, src)
 	})
@@ -71,7 +72,9 @@ func main() {
 		// })
 	})
 
+	//  ---------------------
 	// Output to terminal
+	//  ---------------------
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
@@ -82,21 +85,32 @@ func main() {
 
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Visited", r.Request.URL)
+		if r.Request.URL.String() == "images.squarespace-cdn.com" {
+			fmt.Println("Found image:", r.Request.URL)
+			// Why won't it download????
+			ioutil.WriteFile("images.squarespace-cdn.com"+""+r.Request.URL.String(), r.Body, 0777)
+			fmt.Println("Saving", r.Request.URL)
+		}
+
 	})
 
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished", r.Request.URL)
 	})
 
+	//  ---------------------
 	// Start scraping
+	//  ---------------------
+	// May need a piece of logic to extend to other links
+
 	c.Visit("https://thesislabs.com")
+	c.Visit("https://thesislabs.com/web")
+	c.Visit("https://thesislabs.com/interiors")
+	c.Visit("https://thesislabs.com/fashion")
 
-	// All logic goes here after visit.
-	// Find all image links
-	// appendLinks()
-
-	// Export links to json file
-	// exportToJSON()
+	//  ---------------------
+	// Convert links to JSON
+	//  ---------------------
 
 	linkByTextJSON, err := json.MarshalIndent(linkByText, "", "  ")
 	checkError(err)
@@ -111,4 +125,16 @@ func main() {
 	fmt.Println(string(imagesBySrcJSON))
 
 	ioutil.WriteFile("imagesBySrc.json", linkByTextJSON, 0777)
+
+	//  ---------------------
+	// Download Image Links
+	//  ---------------------
+	for _, image := range images {
+		c.Visit(image)
+	}
+
+	//  ---------------------
+	// Save Images to Disk
+	//  ---------------------
+
 }
