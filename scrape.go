46259efcbef9dac805921a5e254bbbf52eebafe7
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-11 14:20:21 -0700

    [test] output changing, append and write file error

--- scrape.go
+++ scrape.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -41,13 +42,13 @@ func main() {
 
 	// Instantiate default collector
 	c := colly.NewCollector(
-		colly.AllowedDomains("thesislabs.com", "images.squarespace-cdn.com"),
+		colly.AllowedDomains("thesislabs.com", "images.squarespace-cdn.com", "https://thesislabs.com/web", "https://thesislabs.com/interiors", "https://thesislabs.com/fashion"),
 	)
 
 	// Limit Rules to prevent getting banned by the site
 	c.Limit(&colly.LimitRule{
 		DomainGlob: "thesislabs.com*",
-		Delay:      2 * time.Second,
+		Delay:      1 * time.Second,
 	})
 
 	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
@@ -55,6 +56,31 @@ func main() {
 		fmt.Printf("Image found: %q => %s \n", e.Text, src)
 
 		images = append(images, src)
+
+		e.Request.Visit(src)
+		appendLinks = append(appendLinks, Url{Text: e.Text, Link: src})
+
+		os.Create("images.txt")
+
+		linkByText[e.Text] = src
+
+		for k, err := range linkByText {
+			ioutil.WriteFile("images.txt", []byte(src), 0644)
+			fmt.Printf("%s => %s\n", k, err)
+		}
+
+		// f, err := os.OpenFile("images.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		// panic(err)
+		// defer f.Close()
+		// panic(err)
+
+		fmt.Println("~~~~~~~~~~~~~~~~~~~~~~\n")
+		fmt.Printf("%v\n", linkByText)
+		fmt.Printf("%v\n", images)
+		fmt.Printf("%v\n", len(linkByText))
+		fmt.Printf("%v\n", len(images))
+		fmt.Printf("%v\n", cap(images))
+		fmt.Println("~~~~~~~~~~~~~~~~~~~~~~\n")
 	})
 
 	// Find and visit all links
@@ -65,6 +91,7 @@ func main() {
 		linkByText[e.Text] = link
 		absoluteURL := e.Request.AbsoluteURL(link)
 		c.Visit(absoluteURL)
+
 		// appendFile(link)
 		// append(appendLinks, foundLink{
 		// 	Text: e.Text,
@@ -85,11 +112,21 @@ func main() {
 
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Visited", r.Request.URL)
+
 		if r.Request.URL.String() == "images.squarespace-cdn.com" {
 			fmt.Println("Found image:", r.Request.URL)
+			// Append to slice that goes to output.json
+			images = append(images, r.Request.URL.String())
+
 			// Why won't it download????
-			ioutil.WriteFile("images.squarespace-cdn.com"+""+r.Request.URL.String(), r.Body, 0777)
+			ioutil.WriteFile(r.Request.URL.String(), r.Body, 0777)
+			os.Create(r.Request.URL.String() + ".jpg")
 			fmt.Println("Saving", r.Request.URL)
+			if err := os.Rename(r.Request.URL.String(), r.Request.URL.String()+".jpg"); err != nil {
+				panic(err)
+			}
+
+			fmt.Println("Error, not saving", r.Request.URL)
 		}
 
 	})
@@ -101,7 +138,7 @@ func main() {
 	//  ---------------------
 	// Start scraping
 	//  ---------------------
-	// May need a piece of logic to extend to other links
+	// May need a piece of logic to extend to other links instead of hardcoding these :S
 
 	c.Visit("https://thesislabs.com")
 	c.Visit("https://thesislabs.com/web")
@@ -119,10 +156,16 @@ func main() {
 
 	ioutil.WriteFile("output.json", linkByTextJSON, 0777)
 
-	imagesBySrcJSON, err := json.MarshalIndent(images, "", "  ")
-	checkError(err)
+	for _, image := range images {
+		c.Visit(image)
+		imagesBySrcJSON, err := json.MarshalIndent(images, "", "  ")
+		checkError(err)
+		fmt.Println(string(imagesBySrcJSON))
+	}
+	// imagesBySrcJSON, err := json.MarshalIndent(images, "", "  ")
+	// checkError(err)
 
-	fmt.Println(string(imagesBySrcJSON))
+	// fmt.Println(string(imagesBySrcJSON))
 
 	ioutil.WriteFile("imagesBySrc.json", linkByTextJSON, 0777)
 
